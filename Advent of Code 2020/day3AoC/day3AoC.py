cc02b99b45db4fa6cb2de8724770d16b7b416ee6
Sean Hallisey <hallises@oregonstate.edu>
2020-12-14 21:37:43 -0600

    Add part 2 code for day 3. Create function that will check the number of trees encountered for any given slope with the day3AoC input.

--- Advent of Code 2020/day3AoC/day3AoC.py
+++ Advent of Code 2020/day3AoC/day3AoC.py
@@ -2,9 +2,13 @@ path = 'day3AoCinput.txt'
 file = open(path, 'r')
 
 day3_inputs = file.read().splitlines()
-example_inputs = ['..##.......', '#...#...#..', '.#....#..#.', '..#.#...#.#', '.#...##..#.', '..#.##.....', '.#.#.#....#', '.#........#', '#.##...#...', '#...##....#', '.#..#...#.#']
+example_inputs = ['..##.......', '#...#...#..', '.#....#..#.', '..#.#...#.#', '.#...##..#.', '..#.##.....',
+                  '.#.#.#....#', '.#........#', '#.##...#...', '#...##....#', '.#..#...#.#']
 
-def extender(list_input):
+#####PART 1######
+
+
+def extender(list_input, position_to_right):
     """This function will take a list as an input
     that is displayed as the local geology.
 
@@ -13,7 +17,8 @@ def extender(list_input):
     Create new list that the extended geology will be appended.
 
     """
-    times_to_repeat = (int(len(list_input)/3))
+    # times_to_repeat = (int(len(list_input) / position_to_right))
+    times_to_repeat = 100
     print(times_to_repeat)
     extended_list = []
     for line in list_input:
@@ -21,10 +26,11 @@ def extender(list_input):
         extended_list.append(extend)
     return extended_list
 
+
 def check_for_space_or_tree(extender_list):
     """This function takes the extended geology list from the extender function
 
-    
+
     """
     tree_counter = 0
     space_counter = 0
@@ -44,7 +50,69 @@ def check_for_space_or_tree(extender_list):
     return space_counter, tree_counter
 
 
-example = extender(day3_inputs)
-ex_space, ex_tree = check_for_space_or_tree(example)
+# example = extender(day3_inputs, 3)
+# ex_space, ex_tree = check_for_space_or_tree(example)
+#
+# print("Spaces hit: " + str(ex_space), "\nTrees hit: " + str(ex_tree))
 
-print("Spaces hit: " + str(ex_space), "\nTrees hit: " + str(ex_tree))
+#######Part 2##########
+
+def slope_checker(input_list, position_to_right, position_down):
+    """This function will check how many trees are hit with a certain slope.
+
+    The function will take three inputs.
+    The list to input.
+    The position movement to the right for the slope.
+    The position movement down for the slope.
+
+    First I moved the extender inside this function
+    just so I don't have to make a separate function call.
+
+
+
+    """
+
+    # Create new list the extends the 'geology' a certain number of times.
+    times_to_repeat = 100
+    extended_list = []  # This is the new extended geology
+    for line in input_list:
+        extend = line * times_to_repeat
+        extended_list.append(extend)
+
+    tree_counter = 0
+    position_to_move = 0    # This is used later. It will determine the slope in which we are moving
+
+    new_list = ""   # We need to create a new list as a string so we can update the X's and O's.
+    count = 0   # This determines the position down
+    for line in extended_list:
+        s = list(line)  # The line is stored as a list
+        # Position down
+        if count % position_down != 0:  # This determines the down position for the slope.
+            count += 1  # This gives the down movement for the next iteration
+            print(new_list.join(s))     # Print the entire line
+            continue
+        if s[position_to_move] == ".":  # If it is a space
+            s[position_to_move] = "O"   # Update the space as O
+        else:
+            s[position_to_move] = "X"   # If it is a tree
+            tree_counter += 1           # Update the tree as X
+        position_to_move += position_to_right   # This will give us the right movement for the next iteration.
+        count += 1 # This gives the down movement for the next iteration
+        print(new_list.join(s))     # Print the line with the updated X or O
+    return tree_counter     # Return tree_counter.
+
+
+
+r1d1solution = slope_checker(day3_inputs, 1, 1)
+r3d1solution = slope_checker(day3_inputs, 3, 1)
+r5d1solution = slope_checker(day3_inputs, 5, 1)
+r7d1solution = slope_checker(day3_inputs, 7, 1)
+r1d2solution = slope_checker(day3_inputs, 1, 2)
+print(r1d1solution * r3d1solution * r5d1solution * r7d1solution * r1d2solution)
+
+
+
+
+
+
+
